c67b55c558350a8bad1e1b63012c784a652d688e
Yinghao-Li <www6com2010@hotmail.com>
2022-07-09 14:15:37 -0700

    update format text function (without updating version)

--- seqlbtoolkit/text.py
+++ seqlbtoolkit/text.py
@@ -1,4 +1,3 @@
-import re
 import regex
 
 import numpy as np
@@ -7,13 +6,15 @@ from typing import List, Optional
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 
-def format_text(text):
+def format_text(text, remove_ref: Optional[bool] = False, remove_emoj: Optional[bool] = False):
     """
     Normalize text and transform some unicode characters into ascii
 
     Parameters
     ----------
     text: input text string
+    remove_ref: whether remove reference tokens such as [1].
+    remove_emoj: whether remove emojis from the text
 
     Returns
     -------
@@ -23,62 +24,101 @@ def format_text(text):
     # deal with interpuncts
     interpunct = r'[\u00B7\u02D1\u0387\u05BC\u16EB\u2022\u2027\u2218\u2219\u22C5\u23FA' \
                  r'\u25CF\u25E6\u26AB\u2981\u2E30\u2E31\u2E33\u30FB\uA78F\uFF65]'
-    text = re.sub(interpunct, ' ', text)
+    text = regex.sub(interpunct, ' ', text)
 
     # deal with bullets
     bullets = r'[\u2022\u2023\u2043\u204C\u204D\u2219\u25CB\u25D8\u25E6' \
               r'\u2619\u2765\u2767\u29BE\u29BF]'
-    text = re.sub(bullets, ' ', text)
+    text = regex.sub(bullets, ' ', text)
 
     # deal with quotation marks
     s_quotation = r'[\u2018-\u201B\u2039\u203A]'
     d_quotation = r'[\u00AB\u00BB\u201C\u201D\u201E]'
-    text = re.sub(s_quotation, "'", text)
-    text = re.sub(d_quotation, '"', text)
+    text = regex.sub(s_quotation, "'", text)
+    text = regex.sub(d_quotation, '"', text)
 
     # deal with overlay tilde
     tilde = r'[\u0303\u223C\u224B\u02DC\u02F7\u223D\u0360\u0334\u0330\u033E' \
             r'\u1DEC\uFE29\uFE2A\uFE22\uFE23]'
-    text = re.sub(tilde, '~', text)
+    text = regex.sub(tilde, '~', text)
 
     # deal with overlay not tilde
     not_tilde = r'[\u034A]'
-    text = re.sub(not_tilde, '≁', text)
+    text = regex.sub(not_tilde, '≁', text)
 
     text = remove_combining_marks(text)
 
     # deal with "/"
-    text = re.sub(r'[ ]?/[ ]?', '/', text)
+    text = regex.sub(r'[ ]?/[ ]?', '/', text)
 
     # deal with invisible Soft Hyphen
-    text = re.sub(r'[\u00ad]', ' ', text)
+    text = regex.sub(r'[\u00ad]', ' ', text)
     # deal with dash/hyphen
     text = regex.sub(r'[\p{Pd}]', '-', text)
-    text = re.sub(r'[\u2212\uf8ff\uf8fe\ue5f8]', '-', text)
+    text = regex.sub(r'[\u2212\uf8ff\uf8fe\ue5f8]', '-', text)
 
     # deal with `=`
     text = regex.sub(r'[﹦＝]', '=', text)
-    text = re.sub(r"([^ ~≈=≅≤≥⩽⩾<>])([~≈=≅≤≥⩽⩾<>]+)([^ ~≈=≅≤≥⩽⩾<>])", r"\g<1> \g<2> \g<3>", text)
+    text = regex.sub(r"([^ ~≈=≅≤≥⩽⩾<>])([~≈=≅≤≥⩽⩾<>]+)([^ ~≈=≅≤≥⩽⩾<>])", r"\g<1> \g<2> \g<3>", text)
 
     # convert user-defined characters to dash
-    text = re.sub(r'[\uE000-\uF8FF]', '-', text)
+    text = regex.sub(r'[\uE000-\uF8FF]', '-', text)
 
     # deal with repeated comma and period
-    text = re.sub(r'[.]+( *[,.])+', r'..', text)
+    text = regex.sub(r'[.]+( *[,.])+', r'..', text)
 
     # deal with white spaces and \n
     text = regex.sub(r'[\p{Z}]', ' ', text)
-    text = re.sub(r'([ \t]+)?[\r\n]([ \t]+)?', ' ', text)
-    text = re.sub(r'\n+', ' ', text)
-    text = re.sub(r'([ ]{2,})', ' ', text)
+    text = regex.sub(r'([ \t]+)?[\r\n]([ \t]+)?', ' ', text)
+    text = regex.sub(r'\n+', ' ', text)
+    text = regex.sub(r'([ ]{2,})', ' ', text)
 
+    if remove_ref:
+        text = remove_reference(text)
+
+    if remove_emoj:
+        text = remove_emojis(text)
+
+    text = text.strip()
+    return text
+
+
+def remove_emojis(text):
+    """
+    Remove emojis in the text
+    """
+    emoj = regex.compile("["
+                         u"\U0001F600-\U0001F64F"  # emoticons
+                         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                         u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                         u"\U00002500-\U00002BEF"  # chinese char
+                         u"\U00002702-\U000027B0"
+                         u"\U00002702-\U000027B0"
+                         u"\U000024C2-\U0001F251"
+                         u"\U0001f926-\U0001f937"
+                         u"\U00010000-\U0010ffff"
+                         u"\u2640-\u2642"
+                         u"\u2600-\u2B55"
+                         u"\u200d"
+                         u"\u23cf"
+                         u"\u23e9"
+                         u"\u231a"
+                         u"\ufe0f"  # dingbats
+                         u"\u3030]+", regex.UNICODE)
+    return regex.sub(emoj, '', text)
+
+
+def remove_reference(text: str):
+    """
+    Remove reference tokens such as [1] from text
+    """
     # deal with in-sentence references
-    text = re.sub(r' *\[[0-9-, ]+][,-]*', r'', text)
+    text = regex.sub(r' *\[[0-9-, ]+][,-]*', r'', text)
     # deal with after-sentence references
     text = regex.sub(r'([A-Za-zα-ωΑ-Ω\p{Pe}\'"\u2018-\u201d]+[\d-]*)([.])( |[\d]+)([-, ]*[\d]*)*'
                      r'([ ]+[A-Z\dα-ωΑ-Ω\p{Ps}\'"\u2018-\u201d]|$)', r'\g<1>\g<2>\g<5>', text)
 
-    text = text.strip()
     return text
 
 
@@ -96,7 +136,7 @@ def remove_combining_marks(text: str):
     """
     # deal with interpuncts
     diacritics = r'[\u0300-\u036F]'
-    text = re.sub(diacritics, '', text)
+    text = regex.sub(diacritics, '', text)
 
     return text
 
@@ -114,9 +154,9 @@ def substring_mapping(text: str, mapping_dict: dict):
     -------
     string text
     """
-    rep = dict((re.escape(k), v) for k, v in mapping_dict.items())
-    pattern = re.compile("|".join(rep.keys()))
-    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+    rep = dict((regex.escape(k), v) for k, v in mapping_dict.items())
+    pattern = regex.compile("|".join(rep.keys()))
+    text = pattern.sub(lambda m: rep[regex.escape(m.group(0))], text)
     return text
 
 
