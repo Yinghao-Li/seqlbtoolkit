748fb5bff44aa673e3df25e93c1c3f495570a46b
Yinghao-Li <www6com2010@hotmail.com>
2022-09-07 16:44:17 -0400

    update prettify json function

--- seqlbtoolkit/io.py
+++ seqlbtoolkit/io.py
@@ -114,3 +114,24 @@ def init_dir(directory: str):
     remove_dir(directory)
     os.makedirs(os.path.normpath(directory))
     return None
+
+
+def prettify_json(text, indent=2, collapse_level=4):
+    """
+    Make json file more readable by collapsing indent levels higher than `collapse_level`.
+
+    Usage:
+    ```
+    my_instance = list()  # user-defined serializable data structure
+    json_text = json.dumps(my_instance, indent=2, ensure_ascii=False)
+    json_text = prettify_json(json_text, indent=2, collapse_level=4)
+    with open(path_to_file, 'w', encoding='utf=8`) as f:
+        f.write(json_text)
+    ```
+    """
+    pattern = r"[\r\n]+ {%d,}" % (indent * collapse_level)
+    text = re.sub(pattern, ' ', text)
+    text = re.sub(r'([\[({])+ +', r'\g<1>', text)
+    text = re.sub(r'[\r\n]+ {%d}([])}])' % (indent * (collapse_level-1)), r'\g<1>', text)
+    text = re.sub(r'(\S) +([])}])', r'\g<1>\g<2>', text)
+    return text
