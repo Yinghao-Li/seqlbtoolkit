94d11bc8da4caa8a45f42901e6060c3aba860bcc
Yinghao-Li <www6com2010@hotmail.com>
2023-01-24 15:43:41 -0500

    update base dataset

--- seqlbtoolkit/base_model/dataset.py
+++ seqlbtoolkit/base_model/dataset.py
@@ -1,5 +1,5 @@
 import os
-import re
+import regex
 import json
 import logging
 from typing import Optional, List
@@ -24,6 +24,82 @@ logger = logging.getLogger(__name__)
 class DataInstance:
     def __setitem__(self, k, v):
         self.__dict__.update(zip(k, v) if type(k) is tuple else [(k, v)])
+
+
+class BaseDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        super().__init__()
+        # Whether the text and lbs sequences are separated according to maximum BERT input lengths
+        self.data_instances = None
+
+    def __len__(self):
+        return self.data_instances if self.data_instances else 0
+
+    def __getitem__(self, idx):
+        return self.data_instances[idx]
+
+    def prepare(self, config, partition: str):
+        """
+        Load data from disk
+        Parameters
+        ----------
+        config: configurations
+        partition: dataset partition; in [train, valid, test]
+        Returns
+        -------
+        self (MultiSrcNERDataset)
+        """
+        raise NotImplementedError
+
+    def prepare_debug(self, n_inst: Optional[int] = 100):
+        for attr in self.__dict__.keys():
+            if regex.match(f"^_[a-z]", attr):
+                try:
+                    setattr(self, attr, getattr(self, attr)[:n_inst])
+                except TypeError:
+                    pass
+
+        return self
+
+    def save(self, file_path: str):
+        """
+        Save the entire dataset for future usage
+        Parameters
+        ----------
+        file_path: path to the saved file
+        Returns
+        -------
+        self
+        """
+        attr_dict = dict()
+        for attr, value in self.__dict__.items():
+            if regex.match(f"^_[a-z]", attr):
+                attr_dict[attr] = value
+
+        os.makedirs(os.path.dirname(os.path.normpath(file_path)), exist_ok=True)
+        torch.save(attr_dict, file_path)
+
+        return self
+
+    def load(self, file_path: str):
+        """
+        Load the entire dataset from disk
+        Parameters
+        ----------
+        file_path: path to the saved file
+        Returns
+        -------
+        self
+        """
+        attr_dict = torch.load(file_path)
+
+        for attr, value in attr_dict.items():
+            if attr not in self.__dict__:
+                logger.warning(f"Attribute {attr} is not natively defined in dataset!")
+
+            setattr(self, attr, value)
+
+        return self
 
 
 def feature_lists_to_instance_list(instance_class, **kwargs):
@@ -66,7 +142,7 @@ class Batch:
     def __len__(self):
         for v in list(self.__dict__.values()):
             if isinstance(v, torch.Tensor):
-                return v.size(0)
+                return v.shape[0]
 
 
 def load_data_from_json(file_dir: str, config: Optional[BaseNERConfig] = None):
@@ -110,7 +186,7 @@ def load_data_from_json(file_dir: str, config: Optional[BaseNERConfig] = None):
     for i in range(len(data_dict)):
         data = data_dict[str(i)]
         # get tokens
-        tks = [re.sub("[^{}]+".format(printable), "", tk) for tk in data['data']['text']]
+        tks = [regex.sub("[^{}]+".format(printable), "", tk) for tk in data['data']['text']]
         sent_tks = ['[UNK]' if not tk else tk for tk in tks]
         sentence_list.append(sent_tks)
         # get true labels
